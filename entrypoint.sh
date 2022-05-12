f3e3f436ece17db5567594ab6438c3dac1d10bee
Jordan Adler <jordan.m.adler@gmail.com>
2022-05-12 15:23:10 -0700

    iterate

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,23 +4,23 @@ set -e
 
 ssh-keyscan github.com > /root/.ssh/known_hosts
 
-echo "${GITHUB_TOKEN}" >> /root/.ssh/subtree
+echo "${INPUT_GITHUB_TOKEN}" >> /root/.ssh/subtree
 chmod 0600 /root/.ssh/subtree
 
 git config --global --add safe.directory /github/workspace
 
 ARGS=""
 
-if [ "$FORCE" == "true" ]; then
+if [ "${INPUT_FORCE}" == "true" ]; then
 	ARGS="$ARGS -f"
 fi
 
-if [ "$SQUASH" == "true" ]; then
+if [ "${INPUT_SQUASH}" == "true" ]; then
 	ARGS="$ARGS --squash"
 fi
 
-if [ "$MESSAGE" != "" ]; then
+if [ "${INPUT_MESSAGE}" != "" ]; then
 	ARGS="$ARGS --message '${MESSAGE}'"
 fi
 
-git subtree -d -P "${PREFIX}" ${ACTION} ${ARGS} ${REPO} ${POSITION}
+git subtree -d -P "${INPUT_PREFIX}" ${INPUT_ACTION} ${INPUT_ARGS} ${INPUT_REPO} ${INPUT_POSITION}
