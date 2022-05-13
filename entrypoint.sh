97eadc7b469093cdb4df2889805e6e440bca079d
Jordan Adler <jordan.m.adler@gmail.com>
2022-05-12 17:38:49 -0700

    iterate

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,4 +28,6 @@ if [ "${INPUT_MESSAGE}" != "" ]; then
 	ARGS="$ARGS --message ${INPUT_MESSAGE}"
 fi
 
+ls -1 .
+
 git subtree -d -P "${INPUT_PREFIX}" ${INPUT_ACTION} ${ARGS} "${INPUT_REPO}" "${INPUT_POSITION}"
