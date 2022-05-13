3bb1e0bf2b510ff6dbfa47197547d3650050f39a
Jordan Adler <jordan.m.adler@gmail.com>
2022-05-12 17:16:00 -0700

    iterate

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ ssh-keyscan github.com > /root/.ssh/known_hosts
 echo "${INPUT_GITHUB_TOKEN}" >> /root/.ssh/id_github_token
 chmod 0600 /root/.ssh/id_github_token
 
-git config --global --add core.sshCommand "ssh -i ~/.ssh/id_github_token"
+git config --global --add core.sshCommand "ssh -i /root/.ssh/id_github_token"
 git config --global --add safe.directory /github/workspace
 
 ARGS=""
